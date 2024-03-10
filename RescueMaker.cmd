378d0926a8f988335c932df4bceb2e1bac9ae3b1
illsk1lls <gene.rovinsky@gmail.com>
2024-03-10 17:53:21 -0400

    Update RescueMaker.cmd

--- RescueMaker.cmd
+++ RescueMaker.cmd
@@ -2,14 +2,14 @@
 :: Only one instance allowed at a time
 SET "TitleName=RescueMaker"
 TASKLIST /V /NH /FI "imagename eq cmd.exe"|FIND /I /C "%TitleName%">nul
-IF NOT %errorlevel%==1 (ECHO ERROR:&ECHO RescueMaker is already open!) |MSG * & EXIT /b
+IF NOT %errorlevel%==1 POWERSHELL -nop -c "$^={$Notify=[PowerShell]::Create().AddScript({$Audio=New-Object System.Media.SoundPlayer;$Audio.SoundLocation=$env:WinDir + '\Media\Windows Notify System Generic.wav';$Audio.playsync()});$rs=[RunspaceFactory]::CreateRunspace();$rs.ApartmentState="^""STA"^"";$rs.ThreadOptions="^""ReuseThread"^"";$rs.Open();$Notify.Runspace=$rs;$Notify.BeginInvoke()};&$^;$PopUp=New-Object -ComObject Wscript.Shell;$PopUp.Popup("^""RescueMaker is already open!"^"",0,'ERROR:',0x10)">nul&EXIT
 TITLE %TitleName%
 :: Ask for Admin rights
 >nul 2>&1 reg add hkcu\software\classes\.RescueMaker\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %*
 >nul 2>&1 fltmc|| if "%f0%" neq "%~f0" (cd.>"%ProgramData%\runas.RescueMaker" & start "%~n0" /high "%ProgramData%\runas.RescueMaker" "%~f0" "%_:"=""%" & exit /b)
 >nul 2>&1 reg delete hkcu\software\classes\.RescueMaker\ /f &>nul 2>&1 del %ProgramData%\runas.RescueMaker /f /q
 :: Check system - Win11/10 Supported - Both show up as 10
-FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>nul`) DO IF NOT "%%i %%j"=="Windows 10" ECHO. & ECHO Unsupported system detected. & ECHO. & PAUSE & EXIT
+FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>nul`) DO IF NOT "%%i %%j"=="Windows 10" ECHO/ & ECHO Unsupported system detected. & ECHO/ & PAUSE & EXIT
 :: Copy self to %ProgramData% and run from there
 CD /D %~dp0
 IF NOT "%~f0" EQU "%ProgramData%\%~nx0" (
@@ -20,17 +20,17 @@ EXIT /b
 :: Center window
 >nul 2>&1 POWERSHELL -nop -ep Bypass -c "$w=Add-Type -Name WAPI -PassThru -MemberDefinition '[DllImport(\"user32.dll\")]public static extern void SetProcessDPIAware();[DllImport(\"shcore.dll\")]public static extern void SetProcessDpiAwareness(int value);[DllImport(\"kernel32.dll\")]public static extern IntPtr GetConsoleWindow();[DllImport(\"user32.dll\")]public static extern void GetWindowRect(IntPtr hwnd, int[] rect);[DllImport(\"user32.dll\")]public static extern void GetClientRect(IntPtr hwnd, int[] rect);[DllImport(\"user32.dll\")]public static extern void GetMonitorInfoW(IntPtr hMonitor, int[] lpmi);[DllImport(\"user32.dll\")]public static extern IntPtr MonitorFromWindow(IntPtr hwnd, int dwFlags);[DllImport(\"user32.dll\")]public static extern int SetWindowPos(IntPtr hwnd, IntPtr hwndAfterZ, int x, int y, int w, int h, int flags);';$PROCESS_PER_MONITOR_DPI_AWARE=2;try {$w::SetProcessDpiAwareness($PROCESS_PER_MONITOR_DPI_AWARE)} catch {$w::SetProcessDPIAware()}$hwnd=$w::GetConsoleWindow();$moninf=[int[]]::new(10);$moninf[0]=40;$MONITOR_DEFAULTTONEAREST=2;$w::GetMonitorInfoW($w::MonitorFromWindow($hwnd, $MONITOR_DEFAULTTONEAREST), $moninf);$monwidth=$moninf[7] - $moninf[5];$monheight=$moninf[8] - $moninf[6];$wrect=[int[]]::new(4);$w::GetWindowRect($hwnd, $wrect);$winwidth=$wrect[2] - $wrect[0];$winheight=$wrect[3] - $wrect[1];$x=[int][math]::Round($moninf[5] + $monwidth / 2 - $winwidth / 2);$y=[int][math]::Round($moninf[6] + $monheight / 2 - $winheight / 2);$SWP_NOSIZE=0x0001;$SWP_NOZORDER=0x0004;exit [int]($w::SetWindowPos($hwnd, [IntPtr]::Zero, $x, $y, 0, 0, $SWP_NOSIZE -bOr $SWP_NOZORDER) -eq 0)"
 :: Create cache folders
-ECHO.&ECHO Getting Ready...
+ECHO/&ECHO Getting Ready...
 IF EXIST "%~dp0RescueMaker" RD "%~dp0RescueMaker" /S /Q>nul
 MD "%~dp0RescueMaker\Junkbin">nul
 MD "%~dp0RescueMaker\Root">nul
 :: Get 7-Zip - Wimlib-ImageX - SetACL
-CLS&ECHO.&ECHO Getting Utilities...
+CLS&ECHO/&ECHO Getting Utilities...
 PUSHD "%~dp0RescueMaker"&PUSHD "%~dp0RescueMaker\Junkbin"
 POWERSHELL -nop -c "Start-BitsTransfer -Priority Foreground -Source https://www.7-zip.org/a/7zr.exe -Destination '%~dp0RescueMaker\Junkbin\7zr.exe'"; "Start-BitsTransfer -Priority Foreground -Source https://www.7-zip.org/a/7z2300-extra.7z -Destination '%~dp0RescueMaker\Junkbin\7zExtra.7z'"; "Start-BitsTransfer -Priority Foreground -Source https://wimlib.net/downloads/wimlib-1.14.1-windows-x86_64-bin.zip -Destination '%~dp0RescueMaker\Junkbin\wimlib.zip'"; "Start-BitsTransfer -Priority Foreground -Source https://helgeklein.com/downloads/SetACL/current/SetACL%%203.1.2%%20`(executable%%20version`).zip -Destination '%~dp0RescueMaker\Junkbin\SetACL.zip'"
 7zr.exe e -y 7zExtra.7z>nul&7za.exe e -y wimlib.zip libwim-15.dll -r -o..>nul&7za.exe e -y wimlib.zip wimlib-imagex.exe -r -o..>nul&7za.exe e -y SetACL.zip "SetACL (executable version)\64 bit\SetACL.exe" -r -o..>nul&POPD
 :: Find a recovery partition
-ECHO.&ECHO Creating Rescue Media from HostOS...&ECHO.&SET "NOUNMOUNT="&SET "WinRePath=Recovery\WindowsRE"
+ECHO/&ECHO Creating Rescue Media from HostOS...&ECHO/&SET "NOUNMOUNT="&SET "WinRePath=Recovery\WindowsRE"
 FOR /F "usebackq delims=" %%a in (`mountvol^|find "\\"`) do (
 SETLOCAL ENABLEDELAYEDEXPANSION
 CALL :AVAILABLEDRIVELETTERS 1
@@ -50,7 +50,7 @@ IF "!NOUNMOUNT!"=="" MOUNTVOL !L1!: /D>nul
 IF NOT EXIST "%~dp0RescueMaker\Root\Windows\*" (
 CALL :FINDRE
 IF "!R1!"=="" (
-ENDLOCAL &ECHO WARNING! - No recovery partition detected. ^(Try using - reagentc /enable - before proceeding^)&ECHO.&ECHO Aborting process and cleaning up cache folders..&ECHO.&GOTO CLEANUPANDEXIT
+ENDLOCAL &ECHO WARNING! - No recovery partition detected. ^(Try using - reagentc /enable - before proceeding^)&ECHO/&ECHO Aborting process and cleaning up cache folders..&ECHO/&GOTO CLEANUPANDEXIT
 ) ELSE (
 SET L1=!R1!
 GOTO EXTRACT
@@ -59,7 +59,7 @@ GOTO EXTRACT
 ENDLOCAL
 >nul 2>&1 DEL /F "%~dp0RescueMaker\Root\Windows\System32\WallpaperHost.exe"
 :: Configure Rescue Disk
-ECHO.&ECHO Adding Tools...&ECHO.
+ECHO/&ECHO Adding Tools...&ECHO/
 CALL :GETHDDTEST
 CALL :GETCHKDSKGUI
 CALL :GETDISMPLUS
@@ -74,22 +74,22 @@ CLS
 ECHO Loading DISKs...               ^(Target must be USB^)
 ECHO ===================================================
 CALL :LISTDISKS
-ECHO Press ENTER to refresh&ECHO.
+ECHO Press ENTER to refresh&ECHO/
 SET /P USBDISK="Enter the USB DISK # you would like to use (X to Exit): "
 IF "%USBDISK%"=="" GOTO BURNMENU
 IF "%USBDISK%"=="x" SET "USBDISK=X"
-IF "%USBDISK%"=="X" ECHO.&ECHO Aborting media creation and cleaning up cache folders...&ECHO.&GOTO CLEANUPANDEXIT
+IF "%USBDISK%"=="X" ECHO/&ECHO Aborting media creation and cleaning up cache folders...&ECHO/&GOTO CLEANUPANDEXIT
 CALL :DISKEXIST %USBDISK% EXISTS
-IF %EXISTS%==1 ECHO.&ECHO Disk %USBDISK% doesn't exist! &ECHO.&PAUSE&GOTO BURNMENU
+IF %EXISTS%==1 ECHO/&ECHO Disk %USBDISK% doesn't exist! &ECHO/&PAUSE&GOTO BURNMENU
 CALL :CHECKDISK %USBDISK%
 SETLOCAL ENABLEDELAYEDEXPANSION
 CALL :GETDISKTYPE DTYPE2
-IF NOT "!DTYPE2!"=="USB" DEL "%~dp0RescueMaker\currentdisk.diskpart" /F /Q>nul&SET "USBDISK="&ECHO.&ECHO The selected disk is not a USB device. Only USB drives are supported at this time.&ECHO.&PAUSE&ECHO.&GOTO BURNMENU
+IF NOT "!DTYPE2!"=="USB" DEL "%~dp0RescueMaker\currentdisk.diskpart" /F /Q>nul&SET "USBDISK="&ECHO/&ECHO The selected disk is not a USB device. Only USB drives are supported at this time.&ECHO/&PAUSE&ECHO/&GOTO BURNMENU
 CALL :VERIFYDELETE %USBDISK% LASTCHECK
 IF "!LASTCHECK!"=="N" GOTO BURNMENU
 CALL :AVAILABLEDRIVELETTERS 2
 CALL :CONFIGDISKPART %USBDISK% !L1! !L2!
-ECHO Formatting USB...&ECHO.
+ECHO Formatting USB...&ECHO/
 >nul 2>&1 DISKPART /S "%~dp0\RescueMaker\Clean.diskpart"
 TIMEOUT /T 2 /NOBREAK>nul
 >nul 2>&1 DISKPART /S "%~dp0\RescueMaker\Scrubber.diskpart"
@@ -104,18 +104,18 @@ TIMEOUT /T 2 /NOBREAK>nul
 TIMEOUT /T 2 /NOBREAK>nul
 >nul 2>&1 DISKPART /S "%~dp0\RescueMaker\InitBoot.diskpart"
 DEL "%~dp0RescueMaker\*.diskpart" /F /Q
-ECHO Copying files to USB, Please Wait... ^(This may take a few minutes^)&ECHO.
-XCOPY "%~dp0RescueMaker\Root\" "!L1!:\" /E /H /C /I /Y /Z /G /Q&ECHO.
+ECHO Copying files to USB, Please Wait... ^(This may take a few minutes^)&ECHO/
+XCOPY "%~dp0RescueMaker\Root\" "!L1!:\" /E /H /C /I /Y /Z /G /Q&ECHO/
 BCDBOOT !L1!:\Windows /s !L2!: /f ALL /d /addlast
 MOUNTVOL !L2!: /D>nul&SETLOCAL DISABLEDELAYEDEXPANSION
-ECHO.&ECHO Bootable USB Creation Complete! &ECHO.
+ECHO/&ECHO Bootable USB Creation Complete! &ECHO/
 :CLEANUPANDEXIT
 :: Remove cache folders
 POPD&>nul 2>&1 RD "%~dp0RescueMaker" /S /Q&PAUSE
 :: Self delete and exit
 (GOTO) 2>nul & del "%~f0">nul & EXIT
 :LISTDISKS
-(ECHO lis dis)>"%~dp0RescueMaker\list.diskpart"&ECHO.
+(ECHO lis dis)>"%~dp0RescueMaker\list.diskpart"&ECHO/
 FOR /F "usebackq skip=2 tokens=1,2,4,5" %%a in (`DISKPART /S "%~dp0RescueMaker\list.diskpart" ^| FIND "Disk"`) DO (
 CALL :CHECKDISK %%b
 CALL :GETDISKNAME DNAME
@@ -125,7 +125,7 @@ ECHO    Disk #^[%%b^]-^[%%c %%d^]-^[!DTYPE:USB=USB~!^]-^[!DNAME!^]
 ENDLOCAL
 DEL "%~dp0RescueMaker\currentdisk.diskpart" /F /Q>nul
 )
-DEL "%~dp0RescueMaker\list.diskpart" /F /Q&ECHO.
+DEL "%~dp0RescueMaker\list.diskpart" /F /Q&ECHO/
 EXIT /b
 :GETDISKNAME
 FOR /F "usebackq skip=7 delims=" %%a in (`DISKPART /S "%~dp0RescueMaker\currentdisk.diskpart"`) DO (
@@ -143,19 +143,19 @@ EXIT /b
 (ECHO SEL DIS %1&ECHO DETAIL DISK)>"%~dp0RescueMaker\currentdisk.diskpart"
 EXIT/b
 :VERIFYDELETE
-(ECHO lis dis)>"%~dp0RescueMaker\list.diskpart"&ECHO.
+(ECHO lis dis)>"%~dp0RescueMaker\list.diskpart"&ECHO/
 FOR /F "usebackq skip=2 tokens=1,2,4,5" %%a in (`DISKPART /S "%~dp0RescueMaker\list.diskpart" ^| FIND "Disk"`) DO (
 IF %1==%%b (
 CALL :CHECKDISK %%b
 CALL :GETDISKNAME DNAME
 CALL :GETDISKTYPE DTYPE
 SETLOCAL ENABLEDELAYEDEXPANSION
-ECHO    Disk #^[%%b^]-^[%%c %%d^]-^[!DTYPE:USB=USB~!^]-^[!DNAME!^] will be completely erased.&ECHO.
+ECHO    Disk #^[%%b^]-^[%%c %%d^]-^[!DTYPE:USB=USB~!^]-^[!DNAME!^] will be completely erased.&ECHO/
 ENDLOCAL
 CHOICE /C YN /N /M "    Are you sure? This process is irreversable. [Y/N]: "
 IF !errorlevel!==2 SET "%2=N"
 DEL "%~dp0RescueMaker\currentdisk.diskpart" /F /Q>nul
-DEL "%~dp0RescueMaker\list.diskpart" /F /Q&ECHO.
+DEL "%~dp0RescueMaker\list.diskpart" /F /Q&ECHO/
 EXIT /b
 )
 )
